10c7a66b484b87aa11e8de5cc501d0499b4698eb
barrioth <lagia@live.fr>
2026-04-03 22:29:43 +0200

    out of host capacity

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,37 +2,38 @@
 # Création de la VM Pasta Web (Oracle Always Free - Paris)
 # ================================
 
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = var.compartment_id
+  ad_number      = 1
+}
+
 resource "oci_core_instance" "pasta_web_server" {
-  availability_domain = var.availability_domain
+  availability_domain = data.oci_identity_availability_domain.ad.name
   compartment_id      = var.compartment_id
   display_name        = "pasta-web-server"
   shape               = "VM.Standard.A1.Flex"
 
   shape_config {
-    ocpus         = 2
-    memory_in_gbs = 12
+    ocpus         = 1
+    memory_in_gbs = 6
   }
 
-  # Configuration réseau
   create_vnic_details {
     subnet_id        = var.subnet_id
     assign_public_ip = true
   }
 
-  # Image Ubuntu ARM
   source_details {
     source_type = "image"
-    source_id   = var.image_id        # C'est ici qu'on met l'image (et non image_id en top level)
+    source_id   = var.image_id
   }
 
-  # Clé SSH
   metadata = {
-    ssh_authorized_keys = file(var.ssh_public_key_path)
+    ssh_authorized_keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3riGjl6ULQHiKWQYzrI4vUmPcPTCIyQlTzRAIHTck2tulRWpQPUkCDvegW4yN3rwNi2jQC63DZ5nRU+c9EbjnKqI2iYkVaKHrzMPHpAE+wgh2TAe+cQ3wgd2S59zsQCnRYjxteKs36cntQ6fTuJK0eVMc4MX3DowZck/gQCrTeNFL9h8JPSF6LRKYzm0zIrdMxBIJ5NwVoG80N6QVTeoOvU9yhtjUhYt3wF2jeLAQIiieWCo5LCEyvtxspL5kcaAXPS8JY8mG9RkmZhPMx0Ct80WuPEMCiT0W2ypmvvgdAEOpErcJkarOYT6LueS+MoFZbGLxDQv8Psn0KQG/WAl/QIDAQAB"
   }
 
-  # Tags (utilise freeform_tags au lieu de tags)
   freeform_tags = {
-    Name = "pasta-web"
+    Name    = "pasta-web"
     Project = "PROJET"
   }
 }
