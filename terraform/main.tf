6d0090738387beda556a2102437258cf49640f82
barrioth <lagia@live.fr>
2026-04-06 13:56:13 +0200

    modif main.tf ssh

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,8 +33,8 @@ resource "google_compute_instance" "pasta-vm" {
   }
 
   metadata = {
-  ssh-keys = "ubuntu:${file("C:/Users/Lagia/.ssh/id_ed25519_pasta.pub")}\nubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBQiOoTC5e526d4+saIu0E1i9sJ2fEt8Bw8BzltB1TV pasta-deploy"
+    ssh-keys = "ubuntu:${file("C:/Users/Lagia/.ssh/id_ed25519_pasta.pub")}\nubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEBQiOoTC5e526d4+saIu0E1i9sJ2fEt8Bw8BzltB1TV pasta-deploy"
   }
 
-  tags = ["http-server", "ssh"]
+  tags = ["http-server", "https-server", "ssh"]
 }
